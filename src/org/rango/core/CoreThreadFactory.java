221abc45c8684f0088e44f42052e73f3e63335ff
RanGooooo <13835218803@163.com>
2018-11-24 00:54:21 +0800

    init

---
+++ src/org/rango/core/CoreThreadFactory.java
@@ -0,0 +1,13 @@
+package org.rango.core;
+
+import org.rango.core.basics.ThreadFactory;
+
+public class CoreThreadFactory extends ThreadFactory{
+
+
+    @Override
+    public Thread createThreae() {
+
+        return null;
+    }
+}
---
+++ src/org/rango/core/basics/ThreadFactory.java
@@ -0,0 +1,10 @@
+package org.rango.core.basics;
+
+/**
+ * 线程工程
+ */
+public abstract class ThreadFactory {
+
+    public abstract Thread createThreae();
+
+}
